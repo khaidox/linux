12d0932bc73f3b698123eed0394b6ed2eed7f8eb
David Bauer <mail@david-bauer.net>
2022-11-11 13:33:44 +0100

    kconfig: abort configuration on unset symbol
    When a target configuration has unset Kconfig symbols, the build will
    fail when OpenWrt is compiled with V=s and stdin is connected to a tty.
    
    In case OpenWrt is compiled without either of these preconditions, the
    build will succeed with the symbols in question being unset.
    
    Modify the kernel configuration in a way it fails on unset symbols
    regardless of the aforementioned preconditions.
    
    Signed-off-by: David Bauer <mail@david-bauer.net>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -341,6 +341,9 @@ static int conf_askvalue(struct symbol *sym, const char *def)
 		}
 		/* fall through */
 	default:
+		if (!tty_stdio && getenv("FAIL_ON_UNCONFIGURED")) {
+			exit(1);
+		}
 		fflush(stdout);
 		xfgets(line, sizeof(line), stdin);
 		break;
@@ -523,6 +526,9 @@ static int conf_choice(struct menu *menu)
 			}
 			/* fall through */
 		case oldaskconfig:
+			if (!tty_stdio && getenv("FAIL_ON_UNCONFIGURED")) {
+				exit(1);
+			}
 			fflush(stdout);
 			xfgets(line, sizeof(line), stdin);
 			strip(line);
