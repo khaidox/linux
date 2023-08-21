1a81c556f00695ff17ddf6701f70a8d6a68d3cc1
Daniel Golle <daniel@makrotopia.org>
2023-08-21 17:12:44 +0100

    net: ethernet: mtk_eth_soc: fix NULL pointer on hw reset
    When a hardware reset is triggered on devices not initializing WED the
    calls to mtk_wed_fe_reset and mtk_wed_fe_reset_complete dereference a
    pointer on uninitialized stack memory.
    Break out of both functions in case a hw_list entry is 0.
    
    Fixes: 08a764a7c51b ("net: ethernet: mtk_wed: add reset/reset_complete callbacks")
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Acked-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Link: https://lore.kernel.org/r/5465c1609b464cc7407ae1530c40821dcdf9d3e6.1692634266.git.daniel@makrotopia.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/mediatek/mtk_wed.c
+++ drivers/net/ethernet/mediatek/mtk_wed.c
@@ -214,9 +214,13 @@ void mtk_wed_fe_reset(void)
 
 	for (i = 0; i < ARRAY_SIZE(hw_list); i++) {
 		struct mtk_wed_hw *hw = hw_list[i];
-		struct mtk_wed_device *dev = hw->wed_dev;
+		struct mtk_wed_device *dev;
 		int err;
 
+		if (!hw)
+			break;
+
+		dev = hw->wed_dev;
 		if (!dev || !dev->wlan.reset)
 			continue;
 
@@ -237,8 +241,12 @@ void mtk_wed_fe_reset_complete(void)
 
 	for (i = 0; i < ARRAY_SIZE(hw_list); i++) {
 		struct mtk_wed_hw *hw = hw_list[i];
-		struct mtk_wed_device *dev = hw->wed_dev;
+		struct mtk_wed_device *dev;
 
+		if (!hw)
+			break;
+
+		dev = hw->wed_dev;
 		if (!dev || !dev->wlan.reset_complete)
 			continue;
 
